f58e3559508055d4afb114ada316408d73518345
g-agudelo <126741130+g-agudelo@users.noreply.github.com>
2024-03-31 02:06:38 -0500

    Lab_03

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,32 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
+    dic={"cluster":[],"cantidad_de_palabras clave":[],"porcentaje_de_palabras_clave":[],"principales_palabras_clave":[]}
+    with open('clusters_report.txt', mode="r") as cluster:
+        archivo = cluster.readlines()
+    linea2=""
+    for linea in archivo[4:]:
+        regex = r"\b\d+(?:,\d+)?(?:\.\d+)?\s"
+        result = re.findall(regex,linea)
+        if len(result) != 0:
+            dic["cluster"].append(int(result[0].strip()))
+            dic["cantidad_de_palabras clave"].append(int(result[1].strip()))
+            dic["porcentaje_de_palabras_clave"].append(float(result[2].strip().replace(",",".")))
+        regex = r"%\s*(.*?)\n"
+        result = re.findall(regex,linea)
+        if len(result) != 0:
+            linea1 = result[0].replace("  ","")
+        if len(result) == 0:
+            regex = r"\s*(.*?)\n"
+            result = re.findall(regex,linea)
+            if result == [""]:
+                dic["principales_palabras_clave"].append((linea1+linea2).replace(", ",",").replace(",",", "))
+                linea2=""
+            else:
+                linea2=(linea2+result[0]).replace("  ","").replace(".","")
+    df=pd.DataFrame(dic)
+    df["principales_palabras_clave"]=df["principales_palabras_clave"]
     return df
